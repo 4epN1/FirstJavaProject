b65e740053625b5dfeb3e69697151661fe9bccad
4epN1 <chepikov.1994@mail.ru>
2024-03-15 12:13:31 +0300

    New commit 13.03

--- lesson1/src/main/java/com/school/lesson1/MainApp.java
+++ lesson1/src/main/java/com/school/lesson1/MainApp.java
@@ -11,13 +11,29 @@ public class MainApp {
         System.out.println("Task 4");
         compareNumbers();
         System.out.println("Task 5");
-        metodB();
+        methodB();
         System.out.println("Task 6");
-        metodC();
+        methodC();
         System.out.println("Task 7");
-        metodD();
+        methodD();
         System.out.println("Task 8");
-        metodE();
+        methodE();
+        System.out.println("Task 9");
+        int year = 2000;
+        System.out.println(isLeapYear(year));
+        System.out.println("Task 10");
+        methodF();
+        System.out.println("Task 11");
+        methodG();
+        System.out.println("Task 12");
+        /*methodH();*/
+        System.out.println("Task 13");
+        methodI();
+        System.out.println("Task 14");
+        int[] myArray = methodJ(5, 4);
+        for (int i : myArray) {
+            System.out.println(i);
+        }
     }
 
     static void printThreeWords() {
@@ -59,14 +75,14 @@ public class MainApp {
         }
     }
 
-    static void metodB() {
+    static void methodB() {
         int a = 4;
         int b = 6;
         boolean c = (a + b) > 10 && (a + b) <= 20;
         System.out.println(c);
     }
 
-    static void metodC() {
+    static void methodC() {
         int a = -1;
         if (a >= 0) {
             System.out.println("положительное число");
@@ -75,17 +91,74 @@ public class MainApp {
         }
     }
 
-    static void metodD() {
+    static void methodD() {
         int a = 100;
         boolean b = (a > 0);
         System.out.println(b);
     }
 
-    static void metodE() {
+    static void methodE() {
         int a = 0;
         while (a < 10) {
             System.out.println(a + ".Hello!");
             a++;
         }
+    }
+
+    static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void methodF() {
+        int[] myArray = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == 0) {
+                myArray[i] = 1;
+            } else {
+                myArray[i] = 0;
+            }
+        }
+        for (int value : myArray) {
+            System.out.print(value + " ");
+        }
+    }
+
+    static void methodG() {
+        int[] myArray = new int[100];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = i + 1;
+        }
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + " ");
+        }
+    }
+
+    static void methodI() {
+        int counter = 10;
+        int[][] table = new int[counter][counter];
+        for (int i = 0; i < counter; i++) {
+            table[i][i] = 1;
+        }
+        for (int i = 0; i < counter; i++) {
+            for (int j = 0; j < counter; j++) {
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[] methodJ(int len, int initialValue) {
+        int[] myArray = new int[len];
+        for (int i = 0; i < len; i++) {
+            myArray[i] = initialValue;
+        }
+        return myArray;
     }
 }
